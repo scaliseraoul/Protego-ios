40badf11cfd44e567b88f657ffe0cba26c063969
Raoul <scaliseraoul00@gmail.com>
2024-05-01 17:26:00 +0100

    Initial Commit

---
+++ Protego-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Protego-Test
+//
+//  Created by Raoul Scalise on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Protego-Test/Protego_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Protego_TestApp.swift
+//  Protego-Test
+//
+//  Created by Raoul Scalise on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Protego_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
