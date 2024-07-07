fcb69cbb853652613dbe3abdeb71633490eed10c
Raoul <scaliseraoul00@gmail.com>
2024-07-07 18:14:15 +0100

    funzionava con il pmc 15

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Protego-Test' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Protego-Test
+  pod 'TensorFlowLiteSwift'
+  pod 'TensorFlowLiteTaskAudio'
+end
--- Protego-Test/SoundClassifier.swift
+++ Protego-Test/SoundClassifier.swift
@@ -11,24 +11,29 @@ import SoundAnalysis
 import SwiftUI
 import MediaPlayer
 import AudioToolbox
+import TensorFlowLite
 
-class SoundClassifier: NSObject, ObservableObject, SNResultsObserving {
+class SoundClassifier: NSObject, ObservableObject {
     
     private let audioEngine = AVAudioEngine()
-    private var soundClassifier: Protego?
+    private var interpreter: Interpreter!
     private var notificationsManager = NotificationsManager.shared
     private var appState: AppState
     private var audioPlayer: AVAudioPlayer?
     
-    var inputFormat: AVAudioFormat!
-    var analyzer: SNAudioStreamAnalyzer!
     let analysisQueue = DispatchQueue(label: "Raoul.Protego-Test")
+    private let conversionQueue = DispatchQueue(label: "conversionQueue")
+
     
     private var aggressionCount = 0
     
     @Published var classificationResult: String = "Identifying sounds..."
     
     let strobeController = StrobeLightController()
+    
+    private var sampleRate = 16000
+    private let requiredSamples = 15600
+    private var audioDataBuffer = [Int16]()
     
     enum SystemAudioClassificationError: Error {
         case audioStreamInterrupted
@@ -41,6 +46,7 @@ class SoundClassifier: NSObject, ObservableObject, SNResultsObserving {
         do {
             try startAudioSession()
             initializeAudioEngine()
+            initializeInterpreter()
             NotificationCenter.default.addObserver(self, selector: #selector(handleInterruption), name: AVAudioSession.interruptionNotification, object: nil)
             scheduleListeningNotification()
         } catch {
@@ -55,51 +61,116 @@ class SoundClassifier: NSObject, ObservableObject, SNResultsObserving {
     }
     
     private func initializeAudioEngine() {
-        do {
-            soundClassifier = try Protego()
-            inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
-            analyzer = SNAudioStreamAnalyzer(format: inputFormat)
-            let request = try SNClassifySoundRequest(mlModel: soundClassifier!.model)
-            try analyzer.add(request, withObserver: self)
+        let inputNode = audioEngine.inputNode
+        let inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+        
+        
+        guard let recordingFormat = AVAudioFormat(
+          commonFormat: .pcmFormatInt16,
+          sampleRate: Double(sampleRate),
+          channels: 1,
+          interleaved: true
+        ), let formatConverter = AVAudioConverter(from:inputFormat, to: recordingFormat) else { return }
+        
+        let bufferSize = 16000
+        
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: AVAudioFrameCount(bufferSize), format: inputFormat) { [weak self] buffer, time in
             
-            audioEngine.inputNode.installTap(onBus: 0, bufferSize: 8000, format: inputFormat) { [weak self] buffer, time in
-                self?.analysisQueue.async {
-                    self?.analyzer.analyze(buffer, atAudioFramePosition: time.sampleTime)
+            self!.conversionQueue.async {
+              // An AVAudioConverter is used to convert the microphone input to the format required
+              // for the model.(pcm 16)
+              guard let pcmBuffer = AVAudioPCMBuffer(
+                pcmFormat: recordingFormat,
+                frameCapacity: AVAudioFrameCount(bufferSize)
+              ) else { return }
+
+              var error: NSError?
+              let inputBlock: AVAudioConverterInputBlock = { _, outStatus in
+                outStatus.pointee = AVAudioConverterInputStatus.haveData
+                return buffer
+              }
+
+              formatConverter.convert(to: pcmBuffer, error: &error, withInputFrom: inputBlock)
+
+              if let error = error {
+                print(error.localizedDescription)
+                return
+              }
+              if let channelData = pcmBuffer.int16ChannelData {
+                let channelDataValue = channelData.pointee
+                let channelDataValueArray = stride(
+                  from: 0,
+                  to: Int(pcmBuffer.frameLength),
+                  by: buffer.stride
+                ).map { channelDataValue[$0] }
+
+                // Converted pcm 16 values are delegated to the controller.
+                self!.analysisQueue.async {
+                    self!.analyzeBuffer(inputBuffer: channelDataValueArray)
                 }
+              }
             }
+        }
+        
+        do {
             try audioEngine.start()
-            
         } catch {
             print("Error during audio engine setup: \(error)")
         }
     }
     
-    func request(_ request: SNRequest, didProduce result: SNResult) {
-        guard let result = result as? SNClassificationResult,
-              let classification = result.classifications.first,
-              classification.confidence > 0.6 else { return }
+    private func initializeInterpreter() {
+        guard let modelPath = Bundle.main.path(forResource: "sound_classifier_yamnet", ofType: "tflite") else {
+            fatalError("Failed to load model file.")
+        }
+
+        do {
+            interpreter = try Interpreter(modelPath: modelPath)
+            try interpreter!.allocateTensors()
+            let inputTensor = try interpreter.input(at: 0)
+            print("Input tensor shape: \(inputTensor.shape)")
+            print("Input tensor dataType: \(inputTensor.dataType)")
+        } catch {
+            fatalError("Failed to create interpreter: \(error.localizedDescription)")
+        }
+    }
+    
+    private func analyzeBuffer(inputBuffer: [Int16]) {
+        let outputTensor: Tensor
+        do {
+          let audioBufferData = int16ArrayToData(inputBuffer)
+          try interpreter.copy(audioBufferData, toInputAt: 0)
+          try interpreter.invoke()
+
+          outputTensor = try interpreter.output(at: 0)
+        } catch let error {
+          print(">>> Failed to invoke the interpreter with error: \(error.localizedDescription)")
+          return
+        }
+    }
+    
+
+    
+    private func handleClassificationResult(_ results: [Float32]) {
+        guard results.count > 1 else { return }
+        let aggressionConfidence = results[0]
+        let neutralConfidence = results[1]
         
-        print("Got a classification: \(result.classifications)")
-        
-        DispatchQueue.main.async {
-            self.classificationResult = "\(Int(classification.confidence * 100.0))% \(classification.identifier)"
-            
-            let test = false
-            var aggressionThreshold = 2
-            
-            if test {
-                aggressionThreshold = 5
-            }
-            
-            if test || (classification.identifier == "aggression" && classification.confidence > 0.9) {
+        if aggressionConfidence > 0.6 {
+            self.classificationResult = "\(Int(aggressionConfidence * 100.0))% Aggression"
+            if aggressionConfidence > 0.9 {
                 self.aggressionCount += 1
-                if self.aggressionCount >= aggressionThreshold {
-                    self.triggerEmergency()
+                if self.aggressionCount >= 2 {
+                    //self.triggerEmergency()
                     self.aggressionCount = 0
                 }
-            } else {
-                self.aggressionCount = 0
             }
+        } else if neutralConfidence > 0.6 {
+            self.classificationResult = "\(Int(neutralConfidence * 100.0))% Neutral"
+            self.aggressionCount = 0
+        } else {
+            self.classificationResult = "Identifying sounds..."
+            self.aggressionCount = 0
         }
     }
     
@@ -152,7 +223,6 @@ class SoundClassifier: NSObject, ObservableObject, SNResultsObserving {
         }
     }
     
-    
     func stopEmergency() {
         self.appState.shouldNavigateToEmergency = false
         stopEmergencySound()
@@ -174,5 +244,28 @@ class SoundClassifier: NSObject, ObservableObject, SNResultsObserving {
     
     func stopEmergencySound() {
         audioPlayer?.stop()
+    }
+
+    /// Creates a new buffer by copying the buffer pointer of the given `Int16` array.
+    private func int16ArrayToData(_ buffer: [Int16]) -> Data {
+      let floatData = buffer.map { Float($0) / 32768.0 }
+      return floatData.withUnsafeBufferPointer(Data.init)
+    }
+
+    /// Creates a new array from the bytes of the given unsafe data.
+    /// - Returns: `nil` if `unsafeData.count` is not a multiple of `MemoryLayout<Float>.stride`.
+    private func dataToFloatArray(_ data: Data) -> [Float]? {
+      guard data.count % MemoryLayout<Float>.stride == 0 else { return nil }
+
+      #if swift(>=5.0)
+      return data.withUnsafeBytes { .init($0.bindMemory(to: Float.self)) }
+      #else
+      return data.withUnsafeBytes {
+        .init(UnsafeBufferPointer<Float>(
+          start: $0,
+          count: unsafeData.count / MemoryLayout<Element>.stride
+        ))
+      }
+      #endif // swift(>=5.0)
     }
 }
